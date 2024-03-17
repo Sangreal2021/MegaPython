a42a05ed08791340ad2b43e52f562815d726be01
Sangreal2021 <hiw051515@gmail.com>
2024-03-17 12:20:38 +0900

    20240317_ACA

---
+++ pythonProject/P02 데이터타입/Contents/05-int(input()).py
@@ -0,0 +1,30 @@
+'''
+# input()으로 입력 받은 문자열을 타입 변환
+저장받을변수 = 변환할타입(input('입력멘트))
+    - 기본적으로 input()은 문자열로 인식함.
+'''
+
+# 정수로 데이터를 입력받기
+# numA =  int(input('정수 입력: '))
+# # print('numA:', numA, type(numA))
+# print(f'''numA : {numA}
+# type : {type(numA)}
+# ''')
+
+# 실수로 데이터를 입력받기
+numB = float(input('실수 입력: '))
+# print('numB:', numB, type(numB))
+print(f'''numB : {numB}
+type : {type(numB)}
+''')
+
+
+
+
+
+
+
+
+
+
+
