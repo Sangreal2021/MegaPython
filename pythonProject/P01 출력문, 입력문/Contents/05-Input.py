44b7cc2963c77af38ffdf9b532b0dcca1eb14daa
Sangreal2021 <hiw051515@gmail.com>
2024-03-17 10:20:51 +0900

    20240317_ACA

---
+++ pythonProject/P01 출력문, 입력문/Contents/05-Input.py
@@ -0,0 +1,40 @@
+'''
+# 입력문 : input()
+1. 저장할 변수 = input('입력멘트')
+2. input()은 Run창에서 사용자로부터 내용을 입력 받아
+    문자열로 저장함.
+
+'''
+# print('입력하세요 >>> ', end='')
+# a =  input()
+# b = 'Framwork'
+#
+# print(a, b, sep='')
+# print('a :', a)
+
+# a = input('Please Input Message >>> ') # 위의 두 줄과 동일
+# print('a :', a)
+
+# weather = input('오늘 날씨가 어떻습니까? ')
+# print('오늘 날씨는', weather)
+# print()
+
+# 2번 입력받고 출력하기
+id = input('생성할 아이디: ')
+pw = input('생성할 패스워드: ')
+print(id, '님의 패스워드는', pw, '로 생성 되었습니다.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
