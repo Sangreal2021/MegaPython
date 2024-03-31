5d75c02631922d0d1e277a98f4f7c696c4e1088b
Sangreal2021 <hiw051515@gmail.com>
2024-03-31 10:20:19 +0900

    20240331_ACA

--- pythonProject/P04 반복문/Practice_While/Prac05.py
+++ pythonProject/P04 반복문/Practice_While/Prac05.py
@@ -36,8 +36,11 @@ while True:
         print(f'짐의 무게 {num}kg에 대한 수수료는 없습니다.')
     print()
 
-
-
+# # 정삼각형  별찍기
+# n = int(input('삼각형의 높이를 입력하세요 >>> '))
+# for i in range(1, n+1):
+#     print(' ' * (n-i), end='')
+#     print('*' * (2*i - 1))
 
 
 
